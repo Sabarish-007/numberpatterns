77914292b56cc073874d8633b283ff17d2d04040
SABARISH M <sabarish4206@gmail.com>
2025-08-17 02:15:21 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,10 @@
+class code{
+  public static void main(String args[]){
+    for (int i=0;i<=n;i++){
+      for(int j=0;i<=i;j++){
+        System.out.print("*");
+      }
+    }
+    System.out.println(" ");
+  }
+}
